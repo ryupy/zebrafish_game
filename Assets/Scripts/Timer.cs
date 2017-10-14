8858dd40bac12b079215e866e4219a92afffab27
Douhaku.R <ryuuji@christina.local>
2017-10-15 03:47:59 +0900

    version5ed2

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+	private float time = 20;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Text> ().text = time.ToString ("F1");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time -= Time.deltaTime;
+		if (time < 0) {
+			time = 0;
+		}
+		GetComponent<Text> ().text = time.ToString ("F1");
+	}
+}
